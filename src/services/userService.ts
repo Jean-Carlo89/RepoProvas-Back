7e489a957ca7268eb1e8a4864cf7339212216dd3
Jean Carlo <jeancarlomo@hotmail.com>
2021-07-29 11:26:29 -0300

    configuraçao inicial do database e back end

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,7 +5,7 @@ import User from "../entities/User";
 
 export async function getUsers () {
   const users = await getRepository(User).find({
-    select: ["id", "email"]
+    //select: ["id", "email"]
   });
   
   return users;
